9915632a5e29efef4cf1f3bf85d26af05b669a4e
Pavel <pkuptsov@gmail.com>
2015-05-07 11:18:04 +0300

    First

---
+++ mobile/lib/Mobile/Controller/Index.pm
@@ -0,0 +1,12 @@
+package Mobile::Controller::Index;
+use Mojo::Base 'Mojolicious::Controller';
+
+# This action will render a template
+sub welcome {
+  my $self = shift;
+
+  # Render template "example/welcome.html.ep" with message
+  $self->render('index');
+}
+
+1;
