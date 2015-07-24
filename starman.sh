9f5d66550f2251fd6841cfac2673d8b69363c73f
Pavel <pkuptsov@gmail.com>
2015-07-24 13:45:05 +0300

    Add starman script

---
+++ starman.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#plackup --server Starman --workers=5 -D --port 5000  --app /var/www/admin/admin.pl
+
+SITENAME=Gift
+
+DIR=/home/pavel/Gift_mobile/script
+SCRIPT=mobile
+#USER=root
+WORKERS=3
+LISTEN=*:3000
+ERROR_LOG=/home/pavel/Gift_mobile/error_starman.log
+
+PIDFILE=$DIR/site-$SITENAME.pid
+
+
+
+case "$1" in
+  start)
+    plackup --server Starman --workers=$WORKERS -D --listen $LISTEN  \
+    --app $DIR/$SCRIPT --pidfile=$PIDFILE \
+    --pid $PIDFILE \
+    --error-log $ERROR_LOG
+    ;;
+  stop)
+    start-stop-daemon --stop --pidfile $PIDFILE
+    ;;
+  *)
+    echo "Usage: $SCRIPTNAME {start|stop}" >&2
+    exit 3
+    ;;
+esac
